3a702ff4aeb94cb8284d24706d2023784dd69d2d
Daniel Schäfer <ds@struckmeierfliesen.de>
2017-05-14 21:06:41 +0200

    Initial setup React&TypeScript&Material-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react';
 import './App.css';
+import * as injectTapEventPlugin from 'react-tap-event-plugin';
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 const logo = require('./logo.svg');
 
@@ -10,6 +17,9 @@ class App extends React.Component<{}, null> {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <MuiThemeProvider>
+            <RaisedButton label="Button!" />
+          </MuiThemeProvider>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
